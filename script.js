5a8182825848d346b3156ba9f34306eb7b75873e
leandro brito <leandroabrito89@gmail.com>
2020-07-10 15:09:23 -0300

    refactor validation code

--- script.js
+++ script.js
@@ -25,32 +25,50 @@ function isValidEmail(email) {
   return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+  inputArr.forEach(function(input) {
+    // console.log(input.value);
+    if(input.value.trim() === '') {
+      showError(input, `${getFieldName(input)} is required`);
+    } else {
+      showSuccess(input);
+    }
+  });
+}
+
+function getFieldName(input) {
+  return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
 // Event listeners
 form.addEventListener('submit', function(e) {
   e.preventDefault();
-  if(username.value === '') {
-    showError(username, 'Username is required');
-  } else {
-    showSuccess(username);
-  }
+  // if(username.value === '') {
+  //   showError(username, 'Username is required');
+  // } else {
+  //   showSuccess(username);
+  // }
 
-  if(email.value === '') {
-    showError(email, 'Email is required');
-  } else if(!isValidEmail(email.value)) {
-    showError(email, 'Email is not valid');
-  } else {
-    showSuccess(email);
-  }
+  // if(email.value === '') {
+  //   showError(email, 'Email is required');
+  // } else if(!isValidEmail(email.value)) {
+  //   showError(email, 'Email is not valid');
+  // } else {
+  //   showSuccess(email);
+  // }
 
-  if(password.value === '') {
-    showError(password, 'Password is required');
-  } else {
-    showSuccess(password);
-  }
+  // if(password.value === '') {
+  //   showError(password, 'Password is required');
+  // } else {
+  //   showSuccess(password);
+  // }
 
-  if(password2.value === '') {
-    showError(password2, 'Confirm password is required');
-  } else {
-    showSuccess(password2);
-  }
+  // if(password2.value === '') {
+  //   showError(password2, 'Confirm password is required');
+  // } else {
+  //   showSuccess(password2);
+  // }
+
+  checkRequired([username, email, password, password2]);
 });
